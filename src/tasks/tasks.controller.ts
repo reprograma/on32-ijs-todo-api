e6533a1917d8ab692834706d261d2fa22fbc9e93
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2024-06-25 22:23:26 -0300

    put e patch

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly taskService: TasksService){}
+    constructor(private readonly taskService: TasksService) { }
 
     @Post('criar')
-    createTask(@Body() body: {titulo: string; descricao:string}){
+    createTask(@Body() body: { titulo: string; descricao: string }) {
         const task = this.taskService.createTask(body.titulo, body.descricao)
         return {
             statusCode: HttpStatus.CREATED,
@@ -16,10 +17,10 @@ export class TasksController {
     }
 
     @Get()
-    getAll(){
+    getAll() {
         const tasks = this.taskService.getAllTasks();
 
-        return{
+        return {
             statusCode: HttpStatus.OK,
             message: 'Todas as Task retornadas com sucesso',
             data: tasks
@@ -27,17 +28,56 @@ export class TasksController {
     }
 
     @Get(':id')
-    getById(@Param('id') id: string){
+    getById(@Param('id') id: string) {
         const task = this.taskService.getTaskById(id);
 
-        if(!task){
+        if (!task) {
             throw new HttpException('Task não encontrada', HttpStatus.NOT_FOUND)
         }
 
-        return{
+        return {
             statusCode: HttpStatus.OK,
             message: 'Todas as Task retornadas com sucesso',
             data: task
         }
+    }
+
+    @Delete(':id')
+    deleteById(@Param('id') id: string) {
+        this.taskService.deleteTaskById(id);
+        return {
+            statusCode: HttpStatus.NO_CONTENT,
+            message: `Task deletada com sucesso`,
+        };
+    }
+
+    @Put(':id')
+    updateTask(
+        @Param('id') id:string,
+        @Body() body: {titulo:string, descricao:string, status: 'ABERTA' | 'FEITA'}
+    ){
+       const task = this.taskService.updateTask(id, body.titulo, body.descricao, body.status) 
+
+       if(!task) {
+        throw new HttpException('Task não encontrada', HttpStatus.NOT_FOUND)
+       }
+       return{
+        statusCode: HttpStatus.OK,
+        message: 'Task atualizada com sucesso' ,
+        data: task
+       }
+    }
+
+    @Patch('/edit/:id')
+    patchTask(
+        @Param('id') id: string,
+        @Query() updates: Partial<Task>
+    ){
+        const task = this.taskService.patchTask(id, updates)
+        return{
+            statusCode: HttpStatus.OK,
+            message: 'Task atualizada com sucesso' ,
+            data: task
+           }
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,4 +19,45 @@ export class TasksService {
         return this.tasks.find((task) => task.id === id);
     }
 
+    deleteTaskById(id: string): void {
+        // let tasksAtualizadas: Task[] = [];
+        // tasksAtualizadas = this.tasks.filter((task) => task.id !== id);
+        // this.tasks = tasksAtualizadas;
+
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }  
+    
+    updateTask(id:string, titulo: string, descricao: string, status: 'ABERTA' | 'FEITA'): Task{
+        const task = this.getTaskById(id);
+        if(!task) {
+            return null;
+        }
+
+        task.titulo = titulo;
+        task.descricao = descricao;
+        task.status = status;
+
+        return task
+    }
+
+    patchTask(id: string, updates: Partial<Task>): Task{
+        const task = this.getTaskById(id);
+        if(!task) {
+            return null;
+        }
+
+        const campos = ['titulo', 'descricao', 'status'];
+
+        Object.keys(updates).forEach((key) =>{
+            if(campos.includes(key)){
+                task[key] = updates[key]
+            }
+        });
+
+        return task;
+
+    }
+
+    
+
 }
